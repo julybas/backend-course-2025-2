8c368b9a4702bfb92b0c911823c066f384ad89e6
Ярослав bc2025-2 <yaroslav@gmail.com>
2025-09-14 13:23:11 +0300

    Added path and params

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const data = "202408",
   period = "m";
 
 const bankURL = "https://bank.gov.ua/";
-const myURL = new URL("/foo", bankURL);
-// https://example.org/foo
+
+const myURL = new URL(bankURL);
+
+myURL.pathname = "/NBUStatService/v1/statdirectory/banksincexp";
+
+myURL.searchParams.append("date", data);
+myURL.searchParams.append("period", period);
+myURL.searchParams.append("json", "");
+
+console.log(myURL.toString());
 
 //https://bank.gov.ua/NBUStatService/v1/statdirectory/banksincexp?date=202408&period=m&json
