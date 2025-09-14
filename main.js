669d41f9c51f97ec46109940ba1d7c56d5e9a31a
Ярослав bc2025-2 <yaroslav@gmail.com>
2025-09-14 13:01:40 +0300

    Created const data, period and url

---
+++ main.js
@@ -0,0 +1,8 @@
+const data = "202408",
+  period = "m";
+
+const bankURL = "https://bank.gov.ua/";
+const myURL = new URL("/foo", bankURL);
+// https://example.org/foo
+
+//https://bank.gov.ua/NBUStatService/v1/statdirectory/banksincexp?date=202408&period=m&json
